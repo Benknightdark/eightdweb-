580cd5e506789d4c589646b64d10d307ac046535
Kung Ben <Kung Ben>
2017-06-16 10:35:42 +0800

    OK

--- src/app/adminlayout/adminlayout.component.ts
+++ src/app/adminlayout/adminlayout.component.ts
@@ -16,6 +16,7 @@ export class AdminlayoutComponent implements OnInit {
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
       });
+
   }
   onClick(){
   this.account.Logout()
