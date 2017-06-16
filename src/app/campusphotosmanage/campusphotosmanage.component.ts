87449d09fc2fb7ec23a37220f10458fc9e7f104e
Kung Ben <Kung Ben>
2017-06-16 13:57:54 +0800

    s

--- src/app/campusphotosmanage/campusphotosmanage.component.ts
+++ src/app/campusphotosmanage/campusphotosmanage.component.ts
@@ -12,37 +12,52 @@ export class CampusphotosmanageComponent implements OnInit {
   CampusEventPhotos = {
     id: '',
     Name: '',
-    ImageArray: [],
+    ImageUrlArray: [],
     GallaryUrl: "",
     CreateTime: "",
     UpdateTime: ""
   }
-  isFinishSubmit: boolean;
-  constructor() { }
+  ImageArray= []
+  isFinishSubmit: boolean = true;
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
   }
   onSubmit(f) {
+    if (this.ImageArray.length != 0) {
+      //console.log(f)
 
-  }
-  uploadtask() {
-    for (let i = 0; i < this.CampusEventPhotos.ImageArray.length; i++) {
-      firebase.storage().ref().child("/test/" + i + ".jpg").putString(this.CampusEventPhotos.ImageArray[i], 'base64').then((snapshot) => {
-        console.log(snapshot)
-      })
+      this.CampusEventPhotos.id = UUID.UUID();
 
+
+      this.CampusEventPhotos.CreateTime = Date.now().toString();
+      this.CampusEventPhotos.UpdateTime = Date.now().toString();
+      console.log(this.CampusEventPhotos)
+
+      for (let i = 0; i < this.ImageArray.length; i++) {
+        firebase.storage().ref().child("/" + this.CampusEventPhotos.id + "/" + i + ".jpg")
+        .putString(this.ImageArray[i], 'base64').then((snapshot) => {
+          console.log(snapshot)
+        })
+
+      }
+
+    } else {
+      confirm("沒有上傳圖片")
     }
-
   }
+
   imageUploaded(data) {
+    console.log(data)
     //console.log(data)
-    this.CampusEventPhotos.ImageArray.push(data["src"].replace("data:image/jpeg;base64,", ""));
-    console.log(this.CampusEventPhotos.ImageArray)
+    this.ImageArray.push(data["src"].replace("data:image/jpeg;base64,", ""));
+    console.log(this.ImageArray)
     // console.log(this.DMImage)
   }
   imageRemoved(event) {
     // this.MetaFormDes.imageinfo = "";
     //console.log(event)
+    console.log(event)
   }
   disableSendButton(event) {
     //console.log(event)
