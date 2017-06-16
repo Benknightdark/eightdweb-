c9f39ac260d4caecd747e08ff67b29f875f8c3ed
Kung Ben <Kung Ben>
2017-06-16 11:03:28 +0800

    OK

--- src/app/adminlayout/adminlayout.component.ts
+++ src/app/adminlayout/adminlayout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from "app/services/account.service";
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-adminlayout',
@@ -8,18 +9,37 @@ import { AccountService } from "app/services/account.service";
 })
 export class AdminlayoutComponent implements OnInit {
 
-  constructor(private account:AccountService) { }
-
+  constructor(private account: AccountService) { }
+ScreenWidth;
   ngOnInit() {
-      $('.collapsible').collapsible();
-      $('.button-collapse').sideNav({
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav({
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+    Observable.fromEvent(window, 'resize')
+      .map(() => {
+        return document.documentElement.clientWidth;
+      })
+      .subscribe(data => {
+        console.log(data)
+        this.ScreenWidth=data
+        //this.BrowserElement(data)
+      });
+
+    Observable.fromEvent(window, 'load')
+      .map(() => {
+        return document.documentElement.clientWidth;
+      })
+      .subscribe(data => {
+         this.ScreenWidth=data
+        //this.BrowserElement(data)
       });
 
   }
-  onClick(){
-  this.account.Logout()
+
+  onClick() {
+    this.account.Logout()
   }
 
 }
