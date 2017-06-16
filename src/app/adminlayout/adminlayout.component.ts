447d588f4a32071187a030ae2319eddee7c60e15
Kung Ben <Kung Ben>
2017-06-16 09:53:24 +0800

    OK

--- src/app/adminlayout/adminlayout.component.ts
+++ src/app/adminlayout/adminlayout.component.ts
@@ -11,6 +11,11 @@ export class AdminlayoutComponent implements OnInit {
   constructor(private account:AccountService) { }
 
   ngOnInit() {
+      $('.collapsible').collapsible();
+      $('.button-collapse').sideNav({
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      });
   }
   onClick(){
   this.account.Logout()
