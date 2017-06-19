f69d7c405d524647f8d31767e192639c9b78d137
Benknightdark <moon791017@gmail.com>
2017-06-19 10:35:01 +0800

    df

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,7 +1,20 @@
-var cacheName = 'weatherPWA-step-6-1';
+var cacheName = 'eightd';
 var filesToCache = [
-  '/assets',
-  '/index.html'
+  '/',
+  '/assets/images/index.jpg',
+  '/assets/campusdm/1.jpg',
+  '/assets/campusdm/2.jpg',
+  '/assets/campusdm/3.jpg',
+   '/assets/images/background1.jpg',
+    '/assets/images/background2.jpg',
+     '/assets/images/background3.jpg',
+       '/assets/images/slide0.jpg',
+       '/assets/images/slide1.jpg',
+       '/assets/images/slide2.jpg',
+       '/assets/images/slide3.jpg',
+  '/index.html',
+  '/favicon.ico'
+
 
 ];
 
@@ -11,6 +24,28 @@ self.addEventListener('install', function(e) {
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
