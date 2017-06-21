efe9ecd3ea024bc64aa529cdc37d39048f12186e
Kung Ben <Kung Ben>
2017-06-21 16:14:05 +0800

    add admin module and lazy loading

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,22 +8,16 @@ import { CampuseventmanageComponent } from './campuseventmanage/campuseventmanag
 import { CampusphotosmanageComponent } from './campusphotosmanage/campusphotosmanage.component';
 import { AdminlayoutComponent } from './adminlayout/adminlayout.component'
 import { AdminRoutingModule } from './admin-routing.module';
-// import { BrowserModule } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
 import { ImageUploadModule } from 'angular2-image-upload';
-
 import { HttpModule } from '@angular/http';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from "angularfire2/auth";
-
 @NgModule({
   imports: [
- //  BrowserModule,
     CommonModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase),
@@ -31,12 +25,10 @@ import { AngularFireAuthModule } from "angularfire2/auth";
     AngularFireDatabaseModule,
     FormsModule,
     ImageUploadModule.forRoot(),
-  //  BrowserAnimationsModule,
     NgxDatatableModule,
     AdminRoutingModule
   ],
   declarations: [
-
     AdminlayoutComponent,
     CampuseventmanageComponent,
     CampusphotosmanageComponent,
