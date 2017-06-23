f7031144042078e4faa1854f018ff9902f0eda2f
Kung Ben <Kung Ben>
2017-06-23 10:18:08 +0800

    dfas

--- src/app/admin/calendar/calendar.component.ts
+++ src/app/admin/calendar/calendar.component.ts
@@ -1,76 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
-  constructor() { }
+  DefaultDate
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-    $('#calendar').fullCalendar({
-			defaultDate: '2017-05-12',
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2017-05-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2017-05-07',
-					end: '2017-05-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2017-05-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2017-05-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2017-05-11',
-					end: '2017-05-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2017-05-12T10:30:00',
-					end: '2017-05-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2017-05-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2017-05-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2017-05-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2017-05-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2017-05-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2017-05-28'
-				}
-			]
-		});
+    //     $('.datepicker').pickadate({
+    //   selectMonths: true, // Creates a dropdown to control month
+    //   selectYears: 15 // Creates a dropdown of 15 years to control year
+    // });
+    // const DefaultDate = '2017-05-12'
+    this.DefaultDate = new Date()
+    this.ShowCal();
+
   }
+  GetDate() {
+    console.log((moment(this.DefaultDate).format("YYYY-MM")))
+    $('#calendar').fullCalendar('destroy')
+    this.ShowCal();
+  }
+  ShowCal() {
+    this.db.list("AdminEvents/" + moment(this.DefaultDate).format("YYYY-MM")).subscribe(events => {
+      console.log(events)
+      $('#calendar').fullCalendar({
+        defaultDate: moment(this.DefaultDate).format("YYYY-MM-DD"),
+        navLinks: true, // can click day/week names to navigate views
+        viewRender: function (view, element) {
+          console.log(view.currentRange.start._i)
+        },
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: events
+      });
+    })
+  }
+
+
 
 }
