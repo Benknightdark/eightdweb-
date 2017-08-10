a41fb48be7437b46c66233144d768105c01ed72c
Benknightdark <moon791017@gmail.com>
2017-08-10 15:39:02 +0800

    sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,23 @@ export class AppComponent {
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
+
+
     this.sw.updates.subscribe(event => {
       console.log('sw.updates:', event.type);
-
-
+      console.log("version:", event.version)
       if (event.type === 'pending') {
+        // caches.keys().then(k => {
+        //   for (let i = 0; i < k.length; i++) {
+        //     caches.delete(k[i]).then(d => { console.log('delete:', k[i]) })
+        //   }
+        // })
 
-        this.sw.activateUpdate(event.version);
-
-      } else {
-        // event type === 'activation
-        // MGSW is now serving a new version
-        location.reload();
+        this.sw.activateUpdate(event.version).subscribe(au => {
+          console.log("activateupdate:", au)
+          location.reload(true)
+        });
       }
-
 
     });
 
